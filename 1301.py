b5b5f32e7250f70bf93be29688e0b2467de14f68
李钊 <leevick@tsinghua.edu.cn>
2023-02-04 15:27:37 +0800

    ✨ feat(1301): backup question

---
+++ 1301.py
@@ -0,0 +1,10 @@
+#!python3
+from typing import List
+
+
+class Solution:
+    def pathsWithMaxScore(self, board: List[str]) -> List[int]:
+        pass
+
+
+Solution().pathsWithMaxScore(["E23", "2X2", "12S"])
