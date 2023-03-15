781f0fbe740d393ce19c256c0939c581795e079e
李钊 <leevick@tsinghua.edu.cn>
2023-03-15 13:09:20 +0800

    🎉 init: question 1152

---
+++ 1152.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<string> mostVisitedPattern(vector<string> &user,
+                                      vector<int> &time,
+                                      vector<string> &site) {
+                                        
+    }
+};
