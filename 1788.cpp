f12039330001ca91392b64c87b5b2bca6234af08
李钊 <leevick@tsinghua.edu.cn>
2023-03-17 17:52:42 +0800

    ✨ feat: passed

--- 1788.cpp
+++ 1788.cpp
@@ -9,51 +9,18 @@ using namespace std;
 class Solution
 {
 public:
-    struct range
-    {
-        int value;
-        int start;
-        mutable int end;
-        mutable int sum;
-        bool operator<(const range &b) {
-            return this->sum > b.sum;
-        }
-
-        bool operator==(int b) {
-            return this->value == b;
-        }
-    };
-
     int maximumBeauty(vector<int> &flowers) {
         int n = flowers.size();
-        vector<range> ranges;
-        vector<int> sum(n + 1, 0);
+        unordered_map<int, int> hash;
+        int mx = INT_MIN, sum = 0;
 
         for (int i = 0; i < n; ++i) {
             int beauty = flowers[i];
-            sum[i + 1] = sum[i] + beauty;
-            auto r = find(ranges.begin(), ranges.end(), beauty);
-            if (r == ranges.end()) {
-                ranges.push_back({beauty, i, -1});
-            } else {
-                r->end = i;
-                r->sum = sum[i + 1] - sum[r->start];
+            mx = hash.find(beauty) == hash.end() ? mx : max(mx, sum - hash[beauty] + 2 * beauty);
+            sum += beauty > 0 ? beauty : 0;
+            if (hash.find(beauty) == hash.end()) {
+                hash[beauty] = sum;
             }
-        }
-
-        sort(ranges.begin(), ranges.end());
-
-        int mx = INT_MIN;
-        for (auto r : ranges) {
-            if (r.end == -1)
-                continue;
-            int local = r.sum;
-            for (auto i = r.start + 1; i <= r.end - 1; ++i) {
-                if (flowers[i] < 0) {
-                    local = local - flowers[i];
-                }
-            }
-            mx = max(local, mx);
         }
 
         return mx;
